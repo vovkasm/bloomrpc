d1795f714e25757b69afe0bbd205d08a25697ede
Vladimir Timofeev <vovkasm@gmail.com>
2023-09-07 21:08:16 +0200

    cleanups, move app/package.json into root package.json

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -4,36 +4,9 @@
 
 const path = require('path');
 const webpack = require('webpack');
-const fs = require('fs');
-const appDep = require('./app/package.json');
 const pkgDep = require('./package.json');
 
-// Find all the dependencies without a `main` property and add them as webpack externals
-function filterDepWithoutEntryPoints(dep) {
-  // Return true if we want to add a dependency to externals
-  try {
-    // If the root of the dependency has an index.js, return true
-    if (fs.existsSync(path.join(__dirname, `node_modules/${dep}/index.js`))) {
-      return false;
-    }
-    const pgkString = fs
-      .readFileSync(path.join(__dirname, `node_modules/${dep}/package.json`))
-      .toString();
-    const pkg = JSON.parse(pgkString);
-    const fields = ['main', 'module', 'jsnext:main', 'browser'];
-    return !fields.some(field => field in pkg);
-  } catch (e) {
-    console.log(e);
-    return true;
-  }
-}
-
 module.exports = {
-  externals: [
-    ...Object.keys(appDep.externals || {}),
-    ...Object.keys(pkgDep.possibleExternals || {}).filter(filterDepWithoutEntryPoints)
-  ],
-
   module: {
     rules: [
       {
